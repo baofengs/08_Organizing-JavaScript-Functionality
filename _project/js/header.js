516694ecf34a8fc5cbe58a9ffc49564acc5a3217
brzostowski <mateusz@brzostowski.eu>
2017-11-13 12:12:42 +0100

    2.3

--- _project/js/header.js
+++ _project/js/header.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
+  // gwiazka znaczy, znajdź jakikolwiek atrybut z taką właściowścią
+  // $("[red='js-controls'] > [rel*='js-register']")
+  // można też tak:
+  $("[rel*='js-register'], [rel*='js-login]")
+  $("[rel*='js-register']")
 
-  
+  $("[rel='js-controls']").on('click', "[rel*='js-']", function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+    console.log('asddasds');
+  });
 
 });
