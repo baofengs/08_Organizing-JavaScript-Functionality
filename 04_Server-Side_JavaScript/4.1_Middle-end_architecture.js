cc8a6457e18f78e9befe1d6843369b0df2701251
brzostowski <mateusz@brzostowski.eu>
2017-11-23 13:55:06 +0100

    4.1

---
+++ 04_Server-Side_JavaScript/4.1_Middle-end_architecture.js
@@ -0,0 +1,10 @@
+/**
+ * Materiał przedstawia ogólne informacje, na temat architektury aplikacji.
+ * 
+ * Główne wymagania których wcześniej nie można było osiągnąć na froncie:
+ * - dostęp do systemu plików
+ * - routing
+ * - dostęp do HTTP headers
+ * 
+ * Teraz ten problem rozwiązuje node.js
+ */
